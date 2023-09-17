961589fc23287f5e8e9275b256039fb289f4982c
airtonssap-01 <airtonssap@gmail.com>
2023-09-17 17:18:39 -0300

    Carrinho de Compras: criação do produto para o carrinho - incompleto

--- lib/models/cart_manager.dart
+++ lib/models/cart_manager.dart
@@ -1,5 +1,12 @@
+import 'package:libelulas/models/cart_product.dart';
+import 'package:libelulas/models/product.dart';
+
 class CartManager {
 
   List<CartProduct> items = [];
+
+  void addToCart(Product product){
+    items.add(CartProduct.fromProduct(product));
+  }
 
 }
