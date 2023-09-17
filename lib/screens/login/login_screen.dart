e9ce1588c847fe1678efb51296ba1fe7fa6108cd
wwpedro <pedrinhosilva35997@gmail.com>
2023-09-17 15:08:45 -0300

    validação de login feita, lado cliente

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:libelulas/helpers/validators.dart';
 
 class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passController = TextEditingController();
+  final GlobalKey<FormState> formkey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -13,34 +16,48 @@ class LoginScreen extends StatelessWidget {
       body: Center(
         child: Card(
           margin: const EdgeInsets.symmetric(horizontal: 16),
-          child: ListView(
-            padding: const EdgeInsets.all(16),
-            shrinkWrap: true,
-            children: [
-              TextFormField(
-                decoration: const InputDecoration(hintText: "E-mail"),
-                keyboardType: TextInputType.emailAddress,
-                autocorrect: false,
-                validator: (email){
-                  return null;
-                },
-              ),
-              TextFormField(
-                decoration: const InputDecoration(hintText: "Senha"),
-                autocorrect: false,
-                obscureText: true,
-                validator: (pass){
-                  return null;
-                },
-              ),
-              Align(
-                alignment:  Alignment.centerRight,
-                child: TextButton(onPressed: (){
-                  
-                }, child: const Text("esqueci minha senha")),
-              ),
-              ElevatedButton(onPressed: (){}, child: Text("Entrar"),),
-            ],
+          child: Form(
+            key: formkey,
+            child: ListView(
+              padding: const EdgeInsets.all(16),
+              shrinkWrap: true,
+              children: [
+                TextFormField(
+                  controller: emailController,
+                  decoration: const InputDecoration(hintText: "E-mail"),
+                  keyboardType: TextInputType.emailAddress,
+                  autocorrect: false,
+                  validator: (emaill){
+                    if(!emailValid(emaill!))
+                      return 'email valido';
+                    return 'email invalido';
+                  },
+                ),
+                TextFormField(
+                  controller: passController,
+                  decoration: const InputDecoration(hintText: "Senha"),
+                  autocorrect: false,
+                  obscureText: true,
+                  validator: (pass){
+                    if(pass!.isEmpty || pass.length < 6)
+                      return 'email invalida';
+                    return null;
+                  },
+                ),
+                Align(
+                  alignment:  Alignment.centerRight,
+                  child: TextButton(onPressed: (){
+                    
+                  }, child: const Text("esqueci minha senha")),
+                ),
+                ElevatedButton(onPressed: (){
+                  if(formkey.currentState!.validate()){
+                    print(emailController.text);
+                  }
+
+                }, child: Text("Entrar"),),
+              ],
+            ),
           ),
         ),
       ),
