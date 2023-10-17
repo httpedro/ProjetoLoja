ff5d7e0471424664e4065518b8ad54099974dc98
airtonssap-01 <airtonssap@gmail.com>
2023-10-17 19:00:03 -0300

    seleção de tamanho, 'busca' e 'exibição' ok, mas 'selecionando um tamanho' não está totalmente configurado, deixei da forma sem dá erro, mas que não aparenta está selecionado

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,15 +1,31 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:libelulas/models/item_size.dart';
 
-class Product {
+class Product extends ChangeNotifier{
   Product.fromDocument(DocumentSnapshot document){
     id = document.id;
     name = document.get('name') as String;
     description = document.get('description') as String;
     images = List<String>.from(document.get('images') as List<dynamic>);
+    sizes = (document.get('sizes') as List<dynamic>? ?? []).map((s) => ItemSize.fromMap(s as Map<String, dynamic>)).toList();
+
+    print(sizes);
   }
 
   String? id;
   String? name;
   String? description;
   List<String>? images;
+  List<ItemSize>? sizes;
+
+  ItemSize? _selectedSize;
+
+  ItemSize get selectedSize => _selectedSize!;
+
+  set selectedSize(ItemSize value){
+    _selectedSize = value;
+    print(value);
+    notifyListeners();
+  }
 }
--- lib/screens/product/product_screen.dart
+++ lib/screens/product/product_screen.dart
@@ -1,6 +1,8 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:libelulas/models/product.dart';
+import 'package:libelulas/screens/product/components/size_widget.dart';
+import 'package:provider/provider.dart';
 
 class ProductScreen extends StatelessWidget {
 
@@ -13,82 +15,102 @@ class ProductScreen extends StatelessWidget {
 
     final primaryColor = Theme.of(context).primaryColor;
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(product.name as String),
-        centerTitle: true,
-      ),
-      backgroundColor: Colors.white,
-      body: ListView(
-        children: <Widget>[
-          AspectRatio(
-            aspectRatio: 1,
-            child: CarouselSlider(
-              items: product.images!.map((url) {
-                return Image.network(
-                  url,
-                  fit: BoxFit.cover, // Define como a imagem deve se ajustar ao espaço disponível
-                );
-              }).toList(),
-              options: CarouselOptions(
-                height: 200, // Altura do carrossel
-                enlargeCenterPage: true, // Aumenta a imagem do centro
-                viewportFraction: 0.8, // Porcentagem da largura da tela ocupada por cada imagem
-                enableInfiniteScroll: false, // Impede a rolagem infinita
-                autoPlay: false, // Desativa a reprodução automática
+    return ChangeNotifierProvider.value(
+      value: product,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(product.name as String),
+          centerTitle: true,
+        ),
+        backgroundColor: Colors.white,
+        body: ListView(
+          children: <Widget>[
+            AspectRatio(
+              aspectRatio: 1.3,
+              child: CarouselSlider(
+                items: product.images!.map((url) {
+                  return Image.network(
+                    url,
+                    fit: BoxFit.cover, // Define como a imagem deve se ajustar ao espaço disponível
+                  );
+                }).toList(),
+                options: CarouselOptions(
+                  height: 200, // Altura do carrossel
+                  enlargeCenterPage: true, // Aumenta a imagem do centro
+                  viewportFraction: 0.8, // Porcentagem da largura da tela ocupada por cada imagem
+                  enableInfiniteScroll: false, // Impede a rolagem infinita
+                  autoPlay: false, // Desativa a reprodução automática
+                ),
+              )
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    product.name as String,
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top:8),
+                    child: Text(
+                      'A partir de',
+                      style: TextStyle(
+                        color: Colors.grey[600],
+                        fontSize: 13,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    'R\$ 19.99',
+                    style: TextStyle(
+                      fontSize: 22.0,
+                      fontWeight: FontWeight.bold,
+                      color: primaryColor,
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(top: 16, bottom: 8),
+                    child: Text(
+                      'Descrição',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500
+                      ),
+                    ),
+                  ),
+                  Text(
+                    product.description!,
+                    style: const TextStyle(
+                      fontSize: 16
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(top: 16, bottom: 8),
+                    child: Text(
+                      'Tamanhos',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500
+                      ),
+                    ),
+                  ),
+                  Wrap(
+                    spacing: 8,
+                    runSpacing: 8,
+                    children: product.sizes!.map((s){
+                      return SizeWidget(size: s);
+                    }).toList(),
+                  )
+                ],
               ),
             )
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(
-                  product.name as String,
-                  style: const TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w600
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top:8),
-                  child: Text(
-                    'A partir de',
-                    style: TextStyle(
-                      color: Colors.grey[600],
-                      fontSize: 13,
-                    ),
-                  ),
-                ),
-                Text(
-                  'R\$ 19.99',
-                  style: TextStyle(
-                    fontSize: 22.0,
-                    fontWeight: FontWeight.bold,
-                    color: primaryColor,
-                  ),
-                ),
-                const Padding(
-                  padding: EdgeInsets.only(top: 16, bottom: 8),
-                  child: Text(
-                    'Descrição',
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w500
-                    ),
-                  ),
-                ),
-                Text(
-                  product.description!,
-                  style: const TextStyle(
-                    fontSize: 16
-                  ),
-                )
-              ],
-            ),
-          )
-        ],
+          ],
+        ),
       ),
     );
   }
